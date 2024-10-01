215f66ecd5a2b162888ab538681ed97c8e3fe7d1
PR0TEX <protex19@protonmail.com>
2024-10-01 22:10:58 +0200

    feat: adjust data for plant species

--- db/postgres/init.sql
+++ db/postgres/init.sql
@@ -1,23 +1,12 @@
 CREATE TABLE vegetables (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    latin_name VARCHAR(255),
     description TEXT,
-    flowers VARCHAR(255),
-    cycle VARCHAR(80),  -- Increased to fit 67 characters
     maintenance TEXT,
-    leaf_color VARCHAR(100),
-    growth_rate VARCHAR(80),  -- Increased to fit 80 characters
-    fruit VARCHAR(255),
-    type VARCHAR(100),
-    depth_water_requirement VARCHAR(80),  -- Increased to fit 80 characters
-    volume_water_requirement VARCHAR(100),
     watering_period VARCHAR(100),
-    watering_general_benchmark TEXT,
-    wymiary VARCHAR(100),
+    size VARCHAR(100),
     poisonous BOOLEAN,
     default_image TEXT,
-    plant_type VARCHAR(100),
     sunlight VARCHAR(255),
     edible_parts VARCHAR(255)
 );
--- db/postgres/init_db_data.py
+++ db/postgres/init_db_data.py
@@ -20,28 +20,17 @@ for vegetable in data["vegetables"]:
     cur.execute(
         """
         INSERT INTO vegetables 
-        (name, latin_name, description, flowers, cycle, maintenance, leaf_color, growth_rate, fruit, type, depth_water_requirement, volume_water_requirement, watering_period, watering_general_benchmark, wymiary, poisonous, default_image, plant_type, sunlight, edible_parts)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        (name, description, maintenance, watering_period, size, poisonous, default_image, sunlight, edible_parts)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         """,
         (
             vegetable["Name"],
-            vegetable["Latin_name"],
             vegetable["Description"],
-            vegetable["Flowers"],
-            vegetable["Cycle"],
             vegetable["Maintenance"],
-            vegetable["Leaf_color"],
-            vegetable["Growth_rate"],
-            vegetable["Fruit"],
-            vegetable["Type"],
-            vegetable["Depth_water_requirement"],
-            vegetable["Volume_water_requirement"],
             vegetable["Watering_period"],
-            vegetable["Watering_general_benchmark"],
-            vegetable["Wymiary"],
+            vegetable["Size"],
             vegetable["Poisonous"],
             vegetable["Default_image"],
-            vegetable["Plant_type"],
             vegetable["Sunlight"],
             vegetable["Edible_parts"]
         )
