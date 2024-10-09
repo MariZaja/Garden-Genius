f77839aafb11909130e96f79e80a941ce97f4946
Maria Zajaczkowska <maria.zajaczkowska@carrier.com>
2024-10-09 11:01:55 +0200

    GAR-50: Add frontend base

---
+++ apps/frontend/src/pages/user.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export const UserPage: React.FC = () => {
+  return (
+    <Box>
+      <Typography variant="h1">User Page</Typography>
+      {/* Add your user page content here */}
+    </Box>
+  );
+};
