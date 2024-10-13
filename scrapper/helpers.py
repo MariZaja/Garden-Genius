4a0a516034429958e3fa83038057a9135b5dd88e
MalgiGie <malgigie@gmail.com>
2024-10-13 15:40:42 +0200

    Refactor

--- scrapper/helpers.py
+++ scrapper/helpers.py
@@ -1,12 +1,5 @@
 import json
 
-def get_first_x_ids(data, x):
-    if isinstance(data, str):
-        data = json.loads(data)
-
-    ids = [entry['id'] for entry in data['data'][:x]]
-    return ids
-
 def read_key() -> str:
     try:
         with open('KEY.txt', 'r') as file:
@@ -16,3 +9,14 @@ def read_key() -> str:
     except Exception as e:
         print(f"An error occurred while reading the file: {e}")
     return ""
+
+def read_keys() -> list:
+    keys = []
+    try:
+        with open('KEYS.txt', 'r') as file:
+            keys = file.read().splitlines()
+    except FileNotFoundError:
+        print("KEYS.txt file not found.")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+    return keys
--- scrapper/main.py
+++ scrapper/main.py
@@ -1,20 +1,8 @@
 from perenual import get_species_list, get_species_details
-from helpers import get_first_x_ids, read_key
-from species import Species, create_species_from_json
-import json
+from helpers import read_key, read_keys
 
-
-
-def main()-> None:
+def one_key():
     key = read_key()
-
-    #plant_list = get_species_list(key, q="strawberry")
-
-    #species_ids = get_first_x_ids(plant_list, 5)
-
-    # species_details = get_species_details(101, key)
-    # species = create_species_from_json(species_details)
-    # print(f"{species.common_name}")
 
     start_id = int(input("Podaj id gatunku, od którego chcesz zacząć: "))
     if start_id < 1 or start_id > 2901:
@@ -23,12 +11,18 @@ def main()-> None:
     for species_id in range(start_id, start_id + 100):
         species_details = get_species_details(species_id, key)
 
-        # species = create_species_from_json(species_details)
-        # print(species.__dict__)
-        # print(f"{species.common_name}")
+def main()-> None: # refaktoryzacja z chatem: https://chatgpt.com/share/66f86a93-25a8-800c-8b1e-1e77e0f9ac18
+    keys = read_keys()
 
+    for key in keys:
+        print(f"Using key: {key}")
 
+        start_id = int(input(f"Podaj id gatunku, od którego chcesz zacząć dla klucza {key}: "))
+        if start_id < 1 or start_id > 2901:
+            raise ValueError("start_id must be between 1 and 2901")
 
+        for species_id in range(start_id, start_id + 100):
+            get_species_details(species_id, key)
 
 if __name__ == "__main__":
     main()
